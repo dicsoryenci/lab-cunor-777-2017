12c0f0e2ef436ac5141e7cf2a1d0a1176e420146
Geovani de León <erwingeo1021@gmail.com>
2017-01-21 13:42:02 -0600

    Se agrego un botón.

--- practica1/wfaPractica1/wfaPractica1/frmPrincipal.cs
+++ practica1/wfaPractica1/wfaPractica1/frmPrincipal.cs
@@ -16,5 +16,15 @@ namespace wfaPractica1
         {
             InitializeComponent();
         }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
