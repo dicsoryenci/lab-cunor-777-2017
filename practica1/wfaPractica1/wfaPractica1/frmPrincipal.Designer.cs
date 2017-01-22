62fc9262302d930f76f0d9fdfc69169f5cb88da4
Geovani de León <erwingeo1021@gmail.com>
2017-01-21 22:40:07 -0600

    Se añade el algoritmo de recursividad.

--- practica1/wfaPractica1/wfaPractica1/frmPrincipal.Designer.cs
+++ practica1/wfaPractica1/wfaPractica1/frmPrincipal.Designer.cs
@@ -28,35 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtEntrada = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.txtSalida = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(12, 12);
+            this.txtEntrada.Multiline = true;
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(481, 158);
+            this.txtEntrada.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 108);
+            this.button1.Location = new System.Drawing.Point(13, 361);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Probar";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Location = new System.Drawing.Point(13, 176);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(481, 158);
+            this.txtSalida.TabIndex = 2;
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 401);
+            this.Controls.Add(this.txtSalida);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEntrada);
             this.Name = "frmPrincipal";
             this.Text = "Principal";
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TextBox txtEntrada;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSalida;
     }
 }
 
--- practica1/wfaPractica1/wfaPractica1/frmPrincipal.cs
+++ practica1/wfaPractica1/wfaPractica1/frmPrincipal.cs
@@ -22,9 +22,61 @@ namespace wfaPractica1
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
+            string entrada = txtEntrada.Text;
+            List<clsElemento> lstElementos = new List<clsElemento>();
 
+            lstElementos = fncLeer(entrada, lstElementos);
+            foreach (clsElemento ltr in lstElementos)
+            {
+                txtSalida.Text += ltr.letra + " -> " + ltr.cantidad + Environment.NewLine;
+            }
+        }
+
+        public List<clsElemento> fncLeer(string cadena, List<clsElemento> lstLetras) {
+
+            if (cadena != "")
+            {
+                char letra = cadena[0];
+
+                bool encontrado = false;
+                int inxLista = 0;
+                int contador = 0;
+
+                foreach (clsElemento ltr in lstLetras)
+                {
+                    if (ltr.letra == letra) {
+                        encontrado = true;
+                        inxLista = contador;
+                    }
+                    contador++;
+                }
+
+                if (encontrado == false)
+                {
+                    clsElemento m = new clsElemento();
+                    m.letra = letra;
+                    m.cantidad = 0;
+                    lstLetras.Add(m);
+                }
+                else {
+                    clsElemento elemento = lstLetras.ElementAtOrDefault(inxLista);
+                    elemento.cantidad = elemento.cantidad +1;
+                    lstLetras.RemoveAt(inxLista);
+                    lstLetras.Add(elemento);
+                    if (cadena.Length == 1) {
+                        cadena = "";
+                    } else
+                    {
+                        cadena = cadena.Substring(1, cadena.Length-1);
+                    }
+                    
+                }
+
+                fncLeer(cadena, lstLetras);
+            }
+            return lstLetras;
         }
     }
 }
