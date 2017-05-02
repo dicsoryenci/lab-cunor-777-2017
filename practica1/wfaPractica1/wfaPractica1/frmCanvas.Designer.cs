e92c4b6fcae4961b8c7d213c02d858ec169e9c6a
Geovani de León <erwingeo1021@gmail.com>
2017-05-02 08:21:30 -0600

    Modificaciones

--- practica1/wfaPractica1/wfaPractica1/frmCanvas.Designer.cs
+++ practica1/wfaPractica1/wfaPractica1/frmCanvas.Designer.cs
@@ -31,6 +31,7 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +54,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(634, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // frmCanvas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(942, 444);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmCanvas";
@@ -73,5 +85,6 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- practica1/wfaPractica1/wfaPractica1/frmCanvas.cs
+++ practica1/wfaPractica1/wfaPractica1/frmCanvas.cs
@@ -15,22 +15,24 @@ namespace wfaPractica1
         private System.Drawing.Graphics g;
         private int xpos = 0;
         private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Black, 1F);
+        Bitmap i;
         public frmCanvas()
         {
             InitializeComponent();
-            cargar();
+            
         }
 
         private void frmCanvas_Load(object sender, EventArgs e)
         {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
             cargar();
         }
 
         private void cargar() {
+
             g = pictureBox1.CreateGraphics();
             string directory = AppDomain.CurrentDomain.BaseDirectory;
-            Image i = Image.FromFile(directory + "../../imagenes/sprites.png");
-
+           i = new Bitmap(directory + "../../imagenes/sprites.png");
 
             g.DrawImage(i, xpos, 0);
             xpos += 32;
@@ -39,6 +41,15 @@ namespace wfaPractica1
         private void button1_Click(object sender, EventArgs e)
         {
             cargar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var sourceRectangle = new Rectangle(xpos, 64, 32, 32);
+            var destRectangle1 = new Rectangle(xpos, xpos, 32, 32);
+            g.DrawImage(i,sourceRectangle, destRectangle1, GraphicsUnit.Pixel);
+            xpos += 32;
         }
     }
 }
